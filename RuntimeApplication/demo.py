636cf35c079c2306937604e70e34af5fb13782de
jespk77 <jesperk777@gmail.com>
2020-03-24 15:40:08 +0100

    Demo application improvements

--- RuntimeApplication/demo.py
+++ RuntimeApplication/demo.py
@@ -1,38 +1,81 @@
-import sys, pyfprint
+import os, sys, pyfprint
 import cv2, numpy
 
-def save_test_img():
-    """
-        Example code that reads fingerprint and loads the result into opencv
-    """
-    try: pyfprint.open_device()
+images = [f for f in os.listdir(".") if f.startswith("scan") and f.endswith("pgm")]
+count = len(images) + 1
+
+# capture single image, with option of writing to file
+def img_show(args):
+    try: height, width, img = pyfprint.capture_img()
     except Exception as e:
-        print("Failed to open device:", str(e))
+        print("image capture failed:", str(e))
+        return
+
+    if len(img):
+        img = numpy.array(img, numpy.uint8).reshape(height,width)
+        cv2.imshow("Scan", img)
+        print("< Press 's' to save image and close or any other key to close without saving")
+        key = chr(cv2.waitKey(0))
+
+        try:
+            if key == "s":
+                global count
+                file = f"scan{count}.pgm"
+                cv2.imwrite(file, img)
+                count += 1
+
+                images.append(file)
+                print(f"image saved to {file}")
+        except: pass
+        cv2.destroyAllWindows()
+
+# capture a number of images, amount to capture can be specified as argument
+def img_sample(args):
+    try: n = max(int(args[0]), 0)
+    except: n = 5
+    print(f"Getting ready to take a sample of {n} images")
+
+    imgs = []
+    for i in range(n):
+        print(f"Capturing image {i+1}/{n}...")
+        try:
+            height, width, img = pyfprint.capture_img()
+            imgs.append(numpy.array(img, numpy.uint8).reshape(height, width))
+        except: break
+
+    display = numpy.concatenate(imgs, axis=0)
+    cv2.imshow("result", display)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+commands = {
+    "scan": img_show,
+    "c": img_show, # just a 'scan' shortcut
+    "sample": img_sample,
+}
+
+if __name__ == '__main__':
+    print("Initializing...")
+    print(f"< Found {len(images)} images in current directory:")
+    print("  ", " ".join(images))
+
+    print("< Opening fingerprint device...")
+    try: pyfprint.open_device("message")
+    except Exception as e:
+        print("Error! Failed to open device:", str(e))
         sys.exit(-1)
 
-    count = 0
+    print("< Device ready, type 'scan' to capture an image or 'sample {n=5}' to capture n images at once")
     while True:
         try: arg = input("> ")
         except KeyboardInterrupt: arg = "q"
-
         if arg == "q": break
 
-        try: height, width, img = pyfprint.capture_img()
-        except Exception as e:
-            print("image capture failed:", str(e))
-            continue
+        arg = arg.split(" ")
+        if len(arg):
+            cb = commands.get(arg[0])
+            if cb: cb(arg[1:])
 
-        if len(img):
-            img = numpy.array(img, numpy.uint8).reshape(height, width)
-            if arg == "show":
-                cv2.imshow("result", img)
-                cv2.waitKey(0)
-            else:
-                print("image saved")
-                cv2.imwrite(f"test_{count}.pgm", img)
-            count += 1
-
+    print("< Terminating...")
+    print("< Closing fingerprint device...")
     pyfprint.close_device()
-
-if __name__ == '__main__':
-    save_test_img()
